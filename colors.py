ca8990a59cb77f80443536fc4787ff507dd8e019
Bojin Yao <bojinyao@berkeley.edu>
2020-12-06 22:41:04 -0800

    make Color constructor to clamp input ints to [0, 255]

--- colors.py
+++ colors.py
@@ -5,14 +5,14 @@ class Color(namedtuple('Color', ['r', 'g', 'b'])):
 
     def __new__(cls, *args: int) -> None:
         if len(args) == 1:
-            assert 0 <= args[0] <= 255, 'RGB must be between 0 and 255'
             assert type(args[0]) is int, 'input must be an int between 0 and 255'
-            r = g = b = args[0]
+            r = g = b = min(255, max(args[0], 0))
             return super().__new__(cls, r, g, b)
         elif len(args) == 3:
+            args = list(args)
             for i, c in enumerate(args):
-                assert 0 <= c <= 255, f'{i}: {c} must be between 0 and 255'
                 assert type(c) is int, f'{i}: {c} must be an int between 0 and 255'
+                args[i] = min(255, max(args[i], 0))
             return super().__new__(cls, args[0], args[1], args[2])
         else:
             raise ValueError(f'1 or 3 arguments expected, got {len(args)}')
